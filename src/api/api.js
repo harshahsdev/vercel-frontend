54c9be1f8ee931316e6efb06c14f7ef6495ce088
harshahsdev <harsha.dev2025@gmail.com>
2026-05-08 17:22:30 +0530

    route

--- src/api/api.js
+++ src/api/api.js
@@ -11,9 +11,6 @@ API.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
-        console.log("Sending token:", token.substring(0, 20) + "...");
-    } else {
-        console.log("No token found in localStorage");
     }
     return config;
 });
